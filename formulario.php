beee527ec0c59dc126784c65f3d0366ecb69956a
Vicente <vicente.eugenio@hotmail.com>
2014-04-05 12:22:23 -0300

    Formulario ok testado

---
+++ formulario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	
+	<div class="all">
+		<form action="">
+			<input type="text" name="email" value="Seu email" onfocus="this.select()">
+			<input type="submit" value="Enviar">
+		</form>
+	</div>
+</body>
+</html>
